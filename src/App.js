71e53d7ac6cb0d51aa90e4b22b962c279865ffba
mark64-1 <led032337@gmail.com>
2021-03-02 17:06:30 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import ViewMorePage from './containers/ViewMorePage';
+import style from './App.css';
+import Router from './store/Router';
+
+interface Props {}
+
 function App() {
-	return <div className='App'>hello world</div>;
+	return (
+		<div className='App'>
+			<Router/>
+		</div>
+	);
 }
 
 export default App;
