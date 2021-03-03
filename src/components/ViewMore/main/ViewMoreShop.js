b1502f8065a83d717ce28698c5c403485fbfd1ce
mark64-1 <led032337@gmail.com>
2021-03-03 19:11:53 +0900

    fix : default item shop design error fixed

--- src/components/ViewMore/main/ViewMoreShop.js
+++ src/components/ViewMore/main/ViewMoreShop.js
@@ -3,13 +3,12 @@ import ProfileImg from "../common/ProfileImg";
 import SetProfileButton from "../common/SetProfileButton";
 
 const ViewMoreProfile = () => {
-
-    return (
-        <div className="profile">
-            <h1>까미 아이템 상점</h1>
-            <SetProfileButton linkURL="/MyProfilePage" />
-        </div>
-    );
+  return (
+    <div className="shop">
+      <h1>까미 아이템 상점</h1>
+      <textarea value="여기부터 뭘 원하시는지 모르겠어요..." />
+    </div>
+  );
 };
 
 export default ViewMoreProfile;
